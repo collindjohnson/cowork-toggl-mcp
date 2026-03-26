d01a92cac76e98009482bb757c8cbf4d444d22cc
Collin D Johnson <collin@socialswarmmarketing.com>
2026-03-26 01:46:00 -0600

    Add src/constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const TOGGL_API_BASE_URL = "https://api.track.toggl.com/api/v9";
+export const CHARACTER_LIMIT = 25000;
+
+export enum ResponseFormat {
+  MARKDOWN = "markdown",
+  JSON = "json",
+}
